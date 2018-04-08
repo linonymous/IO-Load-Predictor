7e0275a325318b62a051eaef15b428e6f1be7984
Swapnil Walke <iamlinonymous@gmail.com>
2018-04-08 23:31:49 +0530

    - add lstm network

---
+++ model/lstm_model.py
@@ -0,0 +1,72 @@
+from __future__ import print_function
+import pandas as pd
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.optim as optim
+import numpy as np
+
+
+class Sequence(nn.Module):
+    def __init__(self):
+        super(Sequence, self).__init__()
+        self.lstm1 = nn.LSTMCell(1, 51)
+        self.lstm2 = nn.LSTMCell(51, 51)
+        self.linear = nn.Linear(51, 1)
+
+    def forward(self, input, future = 0):
+        outputs = []
+        h_t = Variable(torch.zeros(input.size(0), 51).double(), requires_grad=False)
+        c_t = Variable(torch.zeros(input.size(0), 51).double(), requires_grad=False)
+        h_t2 = Variable(torch.zeros(input.size(0), 51).double(), requires_grad=False)
+        c_t2 = Variable(torch.zeros(input.size(0), 51).double(), requires_grad=False)
+
+        for i, input_t in enumerate(input.chunk(input.size(1), dim=1)):
+            h_t, c_t = self.lstm1(input_t, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+            output = self.linear(h_t2)
+            outputs += [output]
+        for i in range(future):# if we should predict the future
+            h_t, c_t = self.lstm1(output, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+            output = self.linear(h_t2)
+            outputs += [output]
+        outputs = torch.stack(outputs, 1).squeeze(2)
+        return outputs
+
+
+if __name__ == '__main__':
+    # set random seed to 0
+    np.random.seed(0)
+    torch.manual_seed(0)
+
+    # load data and make training set
+    df = pd.read_csv("new_f.csv", dtype="float")
+    data = df.as_matrix()
+    input = Variable(torch.from_numpy(data[3:, :-1]), requires_grad=False)
+    target = Variable(torch.from_numpy(data[3:, 1:]), requires_grad=False)
+    test_input = Variable(torch.from_numpy(data[:3, :-1]), requires_grad=False)
+    test_target = Variable(torch.from_numpy(data[:3, 1:]), requires_grad=False)
+    # build the model
+    seq = Sequence()
+    seq.double()
+    criterion = nn.MSELoss()
+    # LBFGS optimizer
+    optimizer = optim.LBFGS(seq.parameters(), lr=0.8)
+    # training
+    for i in range(20):
+        print('STEP: ', i)
+        def closure():
+            optimizer.zero_grad()
+            out = seq(input)
+            loss = criterion(out, target)
+            print('loss:', loss.data.numpy()[0])
+            loss.backward()
+            return loss
+        optimizer.step(closure)
+        # begin to predict
+        future = 1000
+        pred = seq(test_input, future=future)
+        loss = criterion(pred[:, :-future], test_target)
+        print('test loss:', loss.data.numpy()[0])
+        y = pred.data.numpy()
