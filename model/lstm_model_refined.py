dffbf2d292e986c9d3ffc88f320b12595cb17c6d
linonymous <iamlinonymous@gmail.com>
2018-05-08 21:50:13 +0530

    - add refined_lstm_model.py

---
+++ model/lstm_model_refined.py
@@ -0,0 +1,70 @@
+from __future__ import print_function
+import pandas as pd
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.optim as optim
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+class Sequence(nn.Module):
+    def __init__(self):
+        super(Sequence, self).__init__()
+        self.lstm1 = nn.LSTMCell(6, 51)
+        self.lstm2 = nn.LSTMCell(51, 51)
+        self.linear = nn.Linear(51, 1)
+
+    def forward(self, input, future = 0):
+        outputs = []
+        h_t = Variable(torch.zeros(input.size(0), 51).double(), requires_grad=False)
+        c_t = Variable(torch.zeros(input.size(0), 51).double(), requires_grad=False)
+        h_t2 = Variable(torch.zeros(input.size(0), 51).double(), requires_grad=False)
+        c_t2 = Variable(torch.zeros(input.size(0), 51).double(), requires_grad=False)
+        h_t, c_t = self.lstm1(input, (h_t, c_t))
+        h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+        output = self.linear(h_t2)
+        return output
+
+
+if __name__ == '__main__':
+    # set random seed to 0
+    temp = Variable(torch.DoubleTensor([[[19, 1, 2018, 23, 39, 1]]]))
+    np.random.seed(0)
+    torch.manual_seed(0)
+
+    # load data and make training set
+    df = pd.read_csv("sar.csv", header=None)
+    df[2] = (df[2] - 2018)
+    df[0] = (df[0] - df[0].min()) / (df[0].max() - df[0].min())
+    df[4] = (df[4] - df[4].min()) / (df[4].max() - df[4].min())
+    print(df.head())
+    data = df.as_matrix()
+    input = Variable(torch.from_numpy(data[:-100, :-1]), requires_grad=False)
+    target = Variable(torch.from_numpy(data[:-100, -1:]), requires_grad=False)
+    test_input = Variable(torch.from_numpy(data[-100:, :-1]), requires_grad=False)
+    test_target = Variable(torch.from_numpy(data[-100:, -1:]), requires_grad=False)
+    seq = Sequence()
+    seq.double()
+    criterion = nn.MSELoss()
+    # LBFGS optimizer
+    # optimizer = optim.LBFGS(seq.parameters(), lr=0.8)
+    # training
+    optimizer = optim.LBFGS(seq.parameters(), lr=0.03)
+    for i in range(5000):
+        print('STEP: ', i)
+        def closure():
+            optimizer.zero_grad()
+            out = seq(input)
+            loss = criterion(out, target)
+            print('loss:', loss.data.numpy()[0])
+            loss.backward()
+            return loss
+        optimizer.step(closure)
+
+    pred = seq(test_input)
+    print(pred.data)
+    checkpoint = {
+                  'state_dict': seq.state_dict()}
+    with open('rnn1.net', 'wb') as f:
+        torch.save(checkpoint, f)
